946046cb4f0e6372878721721ccf81b3f674cab5
Boity-Latakgomo <BoityLatakgomo@gmail.com>
2022-10-05 18:53:52 +0200

    app function

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -14,7 +14,13 @@ function TodoList() {
   setTodos(newTodos);
 
  };
-
+const updateTodo = (todoId, newValue) => {
+  if (!todo.text || /^\s*$/.test(todo.text)){
+    return;
+  }
+  setTodos(prev => prev.map(item => (item.id === todoId ? newValue :item))
+  );
+}
  const removeTodo = id => {
   const removeArr = [...todos].filter(todo => todo.id !== id)
   setTodos(removeArr);
@@ -33,7 +39,7 @@ function TodoList() {
     <div>
       <h1> What's the plan for Today?</h1>
       <TodoForm onSubmit={addTodo} />
-      <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} />
+      <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo} />
     </div>
   );
 }
